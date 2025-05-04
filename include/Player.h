06d75f75f1c95e76b240303fee1c9692e3cc6991
Joaquin Aquino <tpja209@gmail.com>
2025-05-04 21:14:54 +0300

    Change robot postion to Vecto2 and all the child classes

--- include/Player.h
+++ include/Player.h
@@ -3,9 +3,7 @@
 #include "Map.h"
 
 class Player : public Robot{
-    private:
-        int playerX = 5;
-        int playerY = 5;
+    
     public:
         Player(int life, const char* skin_path,int pos_x,int pos_y);
         void move_player(Map& world_map);
--- include/Robot.h
+++ include/Robot.h
@@ -5,11 +5,12 @@ class Robot{
         int life;
     protected:
         Texture2D skin;
+        Vector2 position;
         int get_life();
         void set_life(int life);
         
     public:
-        Robot(int life, const char* skin_path);
+        Robot(int life, const char* skin_path,int positionX, int positionY);
         void draw(Vector2 position);
         // ~Robot();
 
